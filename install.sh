946c3bef0b45c8d46588ff411790426a7ea222d1
eeeterii <marwanalknah017@gmail.com>
2025-03-26 17:21:57 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/eeeterii/Eva-source.git
+cd Eva-source
+pip install -r requirements.txt  # إذا كان هناك متطلبات تثبيت
+python main.py  # أو أي أمر آخر لتشغيل المشروع
